77b78d528c0c3b6d7c3dbfe604dc9b1b7e7e48a9
AutomatedTester <dburns@mozilla.com>
2014-01-18 01:06:42 +0000

    zero motors after use so they dont update with old setting

--- src/rogoto/rogoto/rogoto.py
+++ src/rogoto/rogoto/rogoto.py
@@ -9,30 +9,38 @@ BrickPi.MotorEnable[PORT_B] = 1
 def pen_down():
     BrickPi.MotorSpeed[PORT_A] = 100
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_A] = 0
 
 
 def pen_up():
     BrickPi.MotorSpeed[PORT_A] = -100
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_A] = 0
 
 
 def forward(steps):
     BrickPi.MotorSpeed[PORT_B] = steps * 10
     BrickPi.MotorSpeed[PORT_C] = steps * 10
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
+    BrickPi.MotorSpeed[PORT_C] = 0
 
 
 def backward(steps):
     BrickPi.MotorSpeed[PORT_B] = steps * -10
     BrickPi.MotorSpeed[PORT_C] = steps * -10
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
+    BrickPi.MotorSpeed[PORT_C] = 0
 
 
 def left(deg):
     BrickPi.MotorSpeed[PORT_B] = deg
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_B] = 0
 
 
 def right(deg):
     BrickPi.MotorSpeed[PORT_C] = deg
     BrickPiUpdateValues()
+    BrickPi.MotorSpeed[PORT_C] = 0
