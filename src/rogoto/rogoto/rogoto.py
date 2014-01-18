f00426ad7f2095a3d081f9b89d19a08d8dcca739
AutomatedTester <dburns@mozilla.com>
2014-01-18 20:17:52 +0000

    Moving magic numbers to variables so its easier to update

--- src/rogoto/rogoto/rogoto.py
+++ src/rogoto/rogoto/rogoto.py
@@ -1,34 +1,36 @@
 from BrickPi import *
 
 
+speed = 150
+
 BrickPiSetup()
 BrickPi.MotorEnable[PORT_A] = 1
 BrickPi.MotorEnable[PORT_B] = 1
 
 
 def pen_down():
-    BrickPi.MotorSpeed[PORT_A] = 100
+    BrickPi.MotorSpeed[PORT_A] = speed
     BrickPiUpdateValues()
     BrickPi.MotorSpeed[PORT_A] = 0
 
 
 def pen_up():
-    BrickPi.MotorSpeed[PORT_A] = -100
+    BrickPi.MotorSpeed[PORT_A] = -speed
     BrickPiUpdateValues()
     BrickPi.MotorSpeed[PORT_A] = 0
 
 
 def forward(steps):
-    BrickPi.MotorSpeed[PORT_B] = steps * 10
-    BrickPi.MotorSpeed[PORT_C] = steps * 10
+    BrickPi.MotorSpeed[PORT_B] = steps * speed
+    BrickPi.MotorSpeed[PORT_C] = steps * speed
     BrickPiUpdateValues()
     BrickPi.MotorSpeed[PORT_B] = 0
     BrickPi.MotorSpeed[PORT_C] = 0
 
 
 def backward(steps):
-    BrickPi.MotorSpeed[PORT_B] = steps * -10
-    BrickPi.MotorSpeed[PORT_C] = steps * -10
+    BrickPi.MotorSpeed[PORT_B] = steps * -speed
+    BrickPi.MotorSpeed[PORT_C] = steps * -speed
     BrickPiUpdateValues()
     BrickPi.MotorSpeed[PORT_B] = 0
     BrickPi.MotorSpeed[PORT_C] = 0
